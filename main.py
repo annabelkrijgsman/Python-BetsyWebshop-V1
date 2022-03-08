ab4f348f9d3e7be285c1ef0d44f01b5e691108e6
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2022-03-08 15:06:43 +0100

    First draft of all functions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 __winc_id__ = "d7b474e9b3a54d23bca54879a4f1855b"
 __human_name__ = "Betsy Webshop"
 
-# Fill database with test data
+# Create tables and fill database with test data
 def populate_test_data():
     db.create_tables([Tag, Product, User, User_Products, Product_Tag, Transaction])
 
@@ -51,26 +51,81 @@ def populate_test_data():
     Product_Tag.create(product=chair, tag=crafts)
     Product_Tag.create(product=chair, tag=fabric)
 
-# def search(term):
-#     ...
+def search(term):
+    '''Print product name when given term matches a product name or is found in product description'''
+    search_name_query = Product.select().where((Product.name ** f'%{term}%') | (Product.description ** f'%{term}%'))
+    for product in search_name_query:
+        print(product.name)
 
-# def list_user_products(user_id):
-#     ...
+def list_user_products(user_id):
+    '''Print list of products for given user'''
+    user_products = []
+    products = (Product.select()
+                .join(User_Products)
+                .join(User)
+                .where(User.id == user_id)
+    )
+    for product in products:
+        user_products.append(product.name)
 
-# def list_products_per_tag(tag_id):
-#     ...
+    print(user_products)
 
-# def add_product_to_catalog(user_id, product):
-#     ...
+def list_products_per_tag(tag_id):
+    '''Print list of products for given tag'''
+    tag_products = []
+    products = (Product.select()
+                .join(Product_Tag)
+                .join(Tag)
+                .where(Tag.id == tag_id)
+    )
+    for product in products:
+        tag_products.append(product.name)
 
-# def update_stock(product_id, new_quantity):
-#     ...
+    print(tag_products)
 
-# def purchase_product(product_id, buyer_id, quantity):
-#     ...
+def add_product_to_catalog(user_id, product):
+    '''Add product to given user'''
+    get_user = User.get(User.id == user_id)
+    get_product = Product.get(Product.name == product)
+    User_Products.get_or_create(user=get_user, product=get_product)
 
-# def remove_product(user_id, product_id):
-#     ...
+    print(f'{product} added to user')
+
+def update_stock(product_id, new_quantity):
+    '''Update quantity in stock for given product'''
+    update_stock = Product.update(stock=new_quantity).where(Product.id == product_id)
+    update_stock.execute()
+
+    print('Successfully updated stock')
+
+def purchase_product(product_id, buyer_id, quantity):
+    '''Store user instance and product name so both can be used to: add a product to a user's catalog, update quantity of product owned by a user'''
+    user = User.get(User.id == buyer_id)
+    product_name = Product.get(Product.id == product_id).name
+
+    '''Create transaction'''
+    Transaction.create(buyer=buyer_id, purchased_product=product_id, amount_of_purchased_items=quantity, transaction_date=datetime.now())
+
+    '''Update stock after transaction'''
+    purchased_product = Product.get(Product.id == product_id)
+    updated_stock = purchased_product.stock - quantity
+    update_stock(product_id, updated_stock)
+
+    '''Add product to user'''
+    add_product_to_catalog(buyer_id, product_name)
+
+    '''Update quantity of products owned by user'''
+    update_quantity = (User_Products.update(quantity_owned=User_Products.quantity_owned + quantity).where(User_Products.user == user, User_Products.product == product_name))
+    update_quantity.execute()
+
+    print('Successfully updated quantity owned by user')
+
+def remove_product(user_id, product_id):
+    '''Remove product from user if product_id is found in User_Products'''
+    user_product = User_Products.get_or_none(User_Products.user == user_id and User_Products.product == product_id)
+    user_product.delete_instance()
+
+    print('Successfully removed product from user')
 
 if __name__ == "__main__":
     if os.path.exists('betsy-webshop.db') == False:
