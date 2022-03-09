c46bfee1979ab86d37f3452d346898b3ac4d0778
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2022-03-09 14:49:51 +0100

    Search function

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
+from models import *
+from setupdb import *
+from datetime import datetime
+from rich import print
+
 __winc_id__ = "d7b474e9b3a54d23bca54879a4f1855b"
 __human_name__ = "Betsy Webshop"
 
-def search(term):
-    ...
+def search(term: str):
+    term = term.lower()
+    query = Products.select().where(Products.name.contains(term) | Products.description.contains(term))
+    if query:
+        print(f"Your search term [bold cyan]{term}[/bold cyan] has been matched to:")
+        for product in query:
+            print(product.name)
+    else:
+        print(f"[bold red]:exclamation_mark: No products found with {term}[/bold red]")
 
-def list_user_products(user_id):
-    ...
+# def list_user_products(user_id):
+#     ...
 
-def list_products_per_tag(tag_id):
-    ...
+# def list_products_per_tag(tag_id):
+#     ...
 
-# Check check check > fails
-def add_product_to_catalog(user_id, product):
-    ...
+# # Check check check > fails
+# def add_product_to_catalog(user_id, product):
+#     ...
 
-def update_stock(product_id, new_quantity):
-    ...
+# def update_stock(product_id, new_quantity):
+#     ...
 
-# Check check check > fails
-def purchase_product(product_id, buyer_id, quantity):
-    ...
+# # Check check check > fails
+# def purchase_product(product_id, buyer_id, quantity):
+#     ...
 
-def remove_product(user_id, product_id):
-    ...
+# def remove_product(user_id, product_id):
+#     ...
+
+def main():
+    if os.path.exists("betsy-webshop.db") == True:
+        delete_database()
+
+if __name__ == '__main__':
+    main()
